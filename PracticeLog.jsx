40e898af392ee15214d378ab54520bab92651779
Tremah97 <tom.hamer97@gmail.com>
2026-07-06 22:00:22 +0100

    Delete PracticeLog.jsx

--- PracticeLog.jsx
+++
@@ -1,7 +0,0 @@
-import dynamic from "next/dynamic";
-
-const PracticeLog = dynamic(() => import("../components/PracticeLog"), { ssr: false });
-
-export default function PracticePage() {
-  return <PracticeLog />;
-}
